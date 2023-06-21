76dcc37ade1ced8b3c380dbafaffa6cf9987649a
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-06-21 13:20:58 +0100

    Footer Page Built Successfully

--- src/Components/FooterDiv/Footer.jsx
+++ src/Components/FooterDiv/Footer.jsx
@@ -2,7 +2,69 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8]
+    grid grid-cols-5 m-auto item-center justify-center'>
+      <div>
+        <div className='logoDiv'>
+          <h1 className='logo text-[25px] text-white pb-[1.5rem]'>
+            <strong>Job</strong>Search
+          </h1>
+        </div>
+        <p className='text-white pb-[13px] opacity-70 leading-7'>
+          We always make our seekers and companies find the best jobs and employers find 
+          the best candidates.
+        </p>
+      </div>
+
+      <div className='grid pl-[5rem]'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Company
+        </span>
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>About Us</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Features</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>News</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>FAQ</li>
+        </div>
+      </div>
+
+      <div className='grid pl-[5rem]'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Resources
+        </span>
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Account</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Support Center</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Feedback</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Contact Us</li>
+        </div>
+      </div>
+
+      <div className='grid pl-[5rem]'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Support
+        </span>
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Events</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Promo</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Req Demo</li>
+          <li className='text-white opacity-[0.7] hover:opacity-[1]'>Careers</li>
+        </div>
+      </div>
+
+      <div className='grid pl-[5rem]'>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white'>
+          Contact Info
+        </span>
+        <div>
+          <small className='text-[12px] text-white'>
+            getolopadetolu@gmail.com
+          </small>
+          <div className='icons flex gap-4 py-[1rem]'></div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
